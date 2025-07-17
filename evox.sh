d9776ca8c35976f3f8525b0cb7b2df8d5a0c75c2
Sachinpawar86 <106669438+Sachinpawar86@users.noreply.github.com>
2025-07-17 20:42:31 +0530

    Add files via upload

---
+++ evox.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+rm -rf device/xiaomi/mojito
+rm -rf device/xiaomi/sm6150-common
+rm -rf kernel/xiaomi/mojito
+rm -rf hardware/xiaomi
+rm -rf vendor/xiaomi/mojito
+rm -rf vendor/xiaomi/sm6150-common
+
+# ROM Manifest
+repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
+
+rm -rf device/xiaomi/mojito
+rm -rf device/xiaomi/sm6150-common
+rm -rf kernel/xiaomi/mojito
+rm -rf hardware/xiaomi
+rm -rf vendor/xiaomi/mojito
+rm -rf vendor/xiaomi/sm6150-common
+
+# Device
+git clone https://github.com/BKA-16/device_xiaomi_mojito.git -b 16 device/xiaomi/mojito
+git clone https://github.com/BKA-16/device_xiaomi_sm6150-common.git -b 16 device/xiaomi/sm6150-common
+
+# Kernel
+git clone https://github.com/BKA-16/kernel_xiaomi_mojito.git -b inline-rom kernel/xiaomi/mojito
+
+# Hardware
+git clone https://github.com/BKA-16/android_hardware_xiaomi.git -b 16 hardware/xiaomi
+
+# Vendor
+git clone https://github.com/BKA-16/vendor_xiaomi_mojito.git -b 16 vendor/xiaomi/mojito
+git clone https://github.com/BKA-16/vendor_xiaomi_sm6150-common.git -b 16 vendor/xiaomi/sm6150-common
+
+# Sync
+/opt/crave/resync.sh
+
+# Export
+export BUILD_USERNAME=Sachin
+export BUILD_HOSTNAME=crave
+
+# Build Environment
+source build/envsetup.sh
+
+# Lunch
+lunch lineage_mojito-bp2a-userdebug
+
+# Make Cleaninstall
+make installclean
+
+# Build rom
+m evolution
